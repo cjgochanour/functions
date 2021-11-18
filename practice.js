34673debb6fa12dcec9dfadb1fd711cb215b2b75
cjgochanour <cjgochanour@gmail.com>
2021-11-18 13:21:38 -0600

    Addition complete

--- practice.js
+++ practice.js
@@ -10,3 +10,9 @@ const myFavorite = favoriteDog("meow")
 
 console.log("When it comes to pets, " + myFavorite)
 console.log("When it comes to pets, " + favoriteDog("schnauzer"))
+
+const add = (num2, num3, num1) => {
+    return num3 + num2 + num1
+}
+
+console.log(add(17, 4, 11))
